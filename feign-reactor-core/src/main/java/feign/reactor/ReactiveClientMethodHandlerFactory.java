52921add8160d9585a33f8844a7137d8fb014ecb
skarpenko <skarpenko@playtika.com>
2018-08-17 18:44:12 +0300

    rx2

---
+++ feign-reactor-core/src/main/java/feign/reactor/ReactiveClientMethodHandlerFactory.java
@@ -0,0 +1,23 @@
+package feign.reactor;
+
+import feign.MethodMetadata;
+import feign.Target;
+import feign.reactor.client.ReactiveClientFactory;
+
+import static feign.Util.checkNotNull;
+import static feign.reactor.utils.FeignUtils.returnPublisherType;
+
+public class ReactiveClientMethodHandlerFactory implements ReactiveMethodHandlerFactory {
+	private final ReactiveClientFactory reactiveClientFactory;
+
+	public ReactiveClientMethodHandlerFactory(final ReactiveClientFactory reactiveClientFactory) {
+		this.reactiveClientFactory = checkNotNull(reactiveClientFactory, "client must not be null");
+	}
+
+	@Override
+	public ReactiveClientMethodHandler create(Target target, final MethodMetadata metadata) {
+
+		return new ReactiveClientMethodHandler(target, metadata,
+				returnPublisherType(metadata), reactiveClientFactory.apply(metadata));
+	}
+}
--- feign-rx2-reactor/src/main/java/feign/rx2/reactor/ReactiveClientMethodHandler.java
+++ feign-rx2-reactor/src/main/java/feign/rx2/reactor/ReactiveClientMethodHandler.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package feign.reactor;
+package feign.rx2.reactor;
 
 import feign.MethodMetadata;
 import feign.Target;
